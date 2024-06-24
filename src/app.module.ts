c8044f32ced56534b2a5a757ce7353c7ccd17780
Crysthian <129543498+CrysthianKO@users.noreply.github.com>
2024-06-24 16:43:39 -0300

    BOAC-34 feat: Criado modelo de entrada e saída de movimentações financeiras

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { ActionsModule } from './actions/actions.module';
 
 @Module({
-  imports: [UserModule, ActionsModule]
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/boaconta'),
+    UserModule, 
+    ActionsModule],
 })
 export class AppModule {}
