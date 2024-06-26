6dfe64e5f4fe599a2e49f29da6c210bd9549dc79
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2024-06-25 21:10:14 -0300

    BOAC-15 implemented doc '.env' for securite

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,27 @@ import { UserModule } from './user/user.module';
 import { ActionsModule } from './actions/actions.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [UserModule, ActionsModule, AuthModule, 
-    MongooseModule.forRoot('mongodb://localhost:27017/BoaConta')
+    MongooseModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE')
+      }),
+      inject:[ConfigService]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        JWT_SECRET: Joi.string().required(),
+        DATABASE: Joi.string().required()
+      })
+    })
   ]
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,22 +5,25 @@ import { JwtStrategy } from './jwt/jwt.strategy';
 
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './jwt/constants';
 
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/user/schema/user.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     PassportModule.register({defaultStrategy: 'jwt'}),
-    JwtModule.register({
-      global: true,
-      secret:jwtConstants.secret,
-      signOptions: {expiresIn: '5000s'}
-    })
-  ] ,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '5000s' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [JwtStrategy,AuthService],
   exports: [AuthService]
